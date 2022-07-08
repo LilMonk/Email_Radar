d825599523bfc1abf73a4ad1e2867330651a8f98
lilmonk <rahulsahoo66@gmail.com>
2022-07-08 13:37:08 +0530

    Project started.

---
+++ src/main/java/io/spamradar/bootstrap/EmailDataBootstrapServiceApplication.java
@@ -0,0 +1,13 @@
+package io.spamradar.bootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailDataBootstrapServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailDataBootstrapServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/spamradar/bootstrap/EmailDataBootstrapServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spamradar.bootstrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailDataBootstrapServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
